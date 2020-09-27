9b3944c5ed9cdbbb4944c218c132b0d3b5ce77ba
Ivan Nemirich <vania290896@yandex.ru>
2020-09-27 13:41:29 +0700

    js fix

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const popupEdit = document.querySelector('.popup-edit');
 const editButton = document.querySelector('.profile__edit-btn');
 const closePopupEditButton = document.querySelector('.popup-edit__close-btn');
 
-let formElement = document.querySelector('.form-field');
-const submitButton = document.querySelector('.submit-edit-btn');
-let nameOutput = document.querySelector('.profile__name');
-let jobOutput = document.querySelector('.profile__job');
+const nameOutput = document.querySelector('.profile__name');
+const jobOutput = document.querySelector('.profile__job');
 
-let nameInput = document.querySelector('.popup__field_name');
-let jobInput = document.querySelector('.popup__field_job');
+const nameInput = document.querySelector('.popup__field_name');
+const jobInput = document.querySelector('.popup__field_job');
 
 const popupAdd = document.querySelector('.popup-add');
 const addButton = document.querySelector('.profile__add-btn');
@@ -22,6 +20,9 @@ const closePopupImgButton = document.querySelector('.popup-img__close-btn');
 const popupImagePic = document.querySelector('.popup-img__img');
 const popupImageName = document.querySelector('.popup-img__name');
 
+const name = document.querySelector('.popup__field_name-photo');
+const link = document.querySelector('.popup__field_link-photo');
+
 
 
 function copyTextContent() {
@@ -29,25 +30,36 @@ function copyTextContent() {
   jobInput.value = jobOutput.textContent;
 };
 
-function openClosePopupEdit() { 
+function togglePopupEdit() { 
   popupEdit.classList.toggle('popup_opened'); 
   copyTextContent(); 
 };
-editButton.addEventListener('click', openClosePopupEdit);
-closePopupEditButton.addEventListener('click', openClosePopupEdit);
+editButton.addEventListener('click', togglePopupEdit);
+closePopupEditButton.addEventListener('click', togglePopupEdit);
 
 
-function openClosePopupAdd() {
+function togglePopupAdd() {
   popupAdd.classList.toggle('popup_opened');
 };
-addButton.addEventListener('click', openClosePopupAdd);
-closePopupAddButton.addEventListener('click', openClosePopupAdd);
+addButton.addEventListener('click', togglePopupAdd);
+closePopupAddButton.addEventListener('click', togglePopupAdd);
 
 
-function openClosePopupImg() {
+function togglePopupImg() {
   popupImage.classList.toggle('popup_opened');
 };
-closePopupImgButton.addEventListener('click', openClosePopupImg);
+closePopupImgButton.addEventListener('click', togglePopupImg);
+
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  nameOutput.textContent = nameInput.value; 
+  jobOutput.textContent = jobInput.value; 
+
+  togglePopupEdit();
+};
+popupEdit.addEventListener('submit', formSubmitHandler);
 
 
 const initialCards = [
@@ -78,74 +90,50 @@ const initialCards = [
 ];
 
 
-const templateCards = function() {
+function createCard(name, link){
   const cardsTemplate = document.querySelector('.card-template').content;
-  
-  initialCards.forEach(function(item) {
-    const cardsElement = cardsTemplate.cloneNode(true);
-    cardsElement.querySelector('.element__image').src = item.link;
-    cardsElement.querySelector('.element__image').alt = item.name;
-    cardsElement.querySelector('.element__name').textContent = item.name;
-    cardsElement.querySelector('.element__image').addEventListener('click', function() {
-      openClosePopupImg();
-      popupImagePic.src = item.link;
-      popupImagePic.alt = item.name;
-      popupImageName.textContent = item.name;
-    });
+  const cardsElement = cardsTemplate.cloneNode(true);
+  const elementImage = cardsElement.querySelector('.element__image');
+  const elementName = cardsElement.querySelector('.element__name');
 
-    const likeButton = cardsElement.querySelector('.element__like-btn')
-    likeButton.addEventListener('click', function(evt){
-      evt.target.classList.toggle('element__like-btn_active');
-    });
+  elementImage.alt = name;
+  elementName.textContent = name;
+  elementImage.src = link;
 
-      const deleteCard = cardsElement.querySelector('.element__trash');
-      const elementCard = cardsElement.querySelector('.element')
-      deleteCard.addEventListener('click', function(evt){
-        evt.target.closest('.element');
-        elementCard.remove();
-      });
-    cardContainer.append(cardsElement);
+  elementImage.addEventListener('click', function(){
+    togglePopupImg();
+    
+    popupImagePic.src = link;
+    popupImagePic.alt = name;
+    popupImageName.textContent = name;
   });
+
+  const likeButton = cardsElement.querySelector('.element__like-btn');
+  likeButton.addEventListener('click', function(evt){
+    evt.target.classList.toggle('element__like-btn_active');
+  });
+
+  const deleteCard = cardsElement.querySelector('.element__trash');
+  const elementCard = cardsElement.querySelector('.element');
+  deleteCard.addEventListener('click', function(evt){
+    evt.target.closest('.element');
+    elementCard.remove();
+  });
+  return elementCard;
 };
 
-templateCards();
+function addCard(cardContainer, elementCard) {
+  cardContainer.prepend(elementCard);
+}
 
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-
-  nameOutput.textContent = nameInput.value; 
-  jobOutput.textContent = jobInput.value; 
-
-  openClosePopupEdit();
-};
-popupEdit.addEventListener('submit', formSubmitHandler);
-
-function addCard(nameValue, linkValue){
-  const cardTemplate = document.querySelector('.card-template').content;
-  const cardElement = cardTemplate.cloneNode(true);
-
-  cardElement.querySelector('.element__image').alt = nameValue;
-  cardElement.querySelector('.element__name').textContent = nameValue;
-  cardElement.querySelector('.element__image').src = linkValue;
-
-  cardContainer.prepend(cardElement);
-};
+initialCards.forEach(function(item) {
+  addCard(cardContainer, createCard(item.name, item.link));
+});
 
 submitAddButton.addEventListener('click', function(evt) {
   evt.preventDefault();
 
-  const name = document.querySelector('.popup__field_name-photo');
-  const link = document.querySelector('.popup__field_link-photo');
+  addCard(cardContainer, createCard(name.value, link.value));
 
-  addCard(name.value, link.value);
-
-  name.value = '';
-  link.value = '';
-
-  openClosePopupAdd();
+  togglePopupAdd();
 });
-
-
-
-
